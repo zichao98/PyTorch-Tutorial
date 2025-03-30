ca3335279b0a8332c5690b5d55d375d455eec3b2
zichao98 <39187085+zichao98@users.noreply.github.com>
2025-03-30 15:26:37 +0800

    202_variable.py

--- tutorial-contents/202_variable.py
+++ tutorial-contents/202_variable.py
@@ -1,10 +1,3 @@
-"""
-View more, visit my tutorial page: https://mofanpy.com/tutorials/
-My Youtube Channel: https://www.youtube.com/user/MorvanZhou
-
-Dependencies:
-torch: 0.1.11
-"""
 import torch
 from torch.autograd import Variable
 
